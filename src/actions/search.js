bddb18d6d7544fcda32c7c47bb2355eb21b2e41d
Przemek Sobstel <przemek@sobstel.org>
2017-09-26 21:13:15 +0200

    search polishing

--- src/actions/search.js
+++ src/actions/search.js
@@ -9,79 +9,88 @@ export default {
       const text = e.target.value;
 
       if ((e.keyCode === 40) && (state.search.results.length > 0)) { // down arrow
-        actions.search.activeResult(1);
-      } else if ((e.keyCode === 38) && (state.search.results.length > 0)) { // up arrow
-        actions.search.activeResult(-1);
-      } else if (e.keyCode === 27) { // esc
+        actions.search.hoverResult(1);
+        return;
+      }
+
+      if ((e.keyCode === 38) && (state.search.results.length > 0)) { // up arrow
+        actions.search.hoverResult(-1);
+        return;
+      }
+
+      if (e.keyCode === 27) { // esc
         actions.search.onSearchClearClick();
-      } else if (e.keyCode === 13) { // enter
+        return;
+      }
+
+      if (e.keyCode === 13) { // enter
         const activeItem = state.search.results.find(result => result.active === true);
         actions.router.go(`/c/${activeItem.id}`);
         actions.search.exitSearch();
-      } else {
-        terminateDelay(delay);
+        return;
+      }
 
-        // restart
+      // restart
+      terminateDelay(delay);
+      update(prevState => (
+        { ...prevState, search: { ...prevState.search, resultsHint: null, value: text } }
+      ));
+
+      if (text.length === 0) {
+        const historyResults = History.all().slice(0, MAX_RESULTS);
+
         update(prevState => (
-          { ...prevState, search: { ...prevState.search, resultsHint: null, value: text } }
+          { ...prevState, search: { ...prevState.search, results: historyResults } }
         ));
+        return;
+      }
 
-        if (text.length === 0) {
-          const historyResults = History.all().slice(0, MAX_RESULTS);
+      if (text.length > 0) {
+        update(prevState => (
+          { ...prevState, search: { ...prevState.search, clearButtonVisible: true } }
+        ));
+      }
 
-          update(prevState => (
-            { ...prevState, search: { ...prevState.search, results: historyResults } }
-          ));
+      const historyResults = History.search(text).slice(0, MAX_RESULTS);
+      if (historyResults.length > 0) {
+        update(prevState => (
+          { ...prevState, search: { ...prevState.search, results: historyResults } }
+        ));
+      }
+
+      if (historyResults.length < MAX_RESULTS) {
+        if (text.length < 4) {
+          const resultsHint = 'type 4 letters or more to search full database...';
+          update(prevState => ({ ...prevState, search: { ...prevState.search, resultsHint } }));
           return;
         }
 
-        if (text.length > 0) {
-          update(prevState => (
-            { ...prevState, search: { ...prevState.search, clearButtonVisible: true } }
-          ));
-        }
+        update(prevState => ({ ...prevState, search: { ...prevState.search, loading: true } }));
 
-        const historyResults = History.search(text).slice(0, MAX_RESULTS);
-        if (historyResults.length > 0) {
-          update(prevState => (
-            { ...prevState, search: { ...prevState.search, results: historyResults } }
-          ));
-        }
+        delay(0.25, () => {
+          request(`/search?q=${text}`, (results) => {
+            let hint = null;
+            if (results.length === 0 && historyResults.length === 0) {
+              hint = 'no results found';
+            }
 
-        if (historyResults.length < MAX_RESULTS) {
-          if (text.length < 4) {
-            const resultsHint = 'type 4 letters or more to search full database...';
-            update(prevState => ({ ...prevState, search: { ...prevState.search, resultsHint } }));
-            return;
-          }
+            update((prevState) => {
+              // Set is here to ensure uniqueness and order
+              const mergedResults = uniqBy(historyResults.concat(results), 'id')
+                .slice(0, MAX_RESULTS);
 
-          update(prevState => ({ ...prevState, search: { ...prevState.search, loading: true } }));
-
-          delay(0.25, () => {
-            request(`/search?q=${text}`, (results) => {
-              let hint = null;
-              if (results.length === 0 && historyResults.length === 0) {
-                hint = 'no results found';
-              }
-
-              update((prevState) => {
-                // Set is here to ensure uniqueness and order
-                const mergedResults = uniqBy(historyResults.concat(results), 'id')
-                  .slice(0, MAX_RESULTS);
-
-                return {
-                  ...prevState,
-                  search: {
-                    ...prevState.search,
-                    hint,
-                    loading: false,
-                    results: mergedResults,
-                  },
-                };
-              });
+              return {
+                ...prevState,
+                search: {
+                  ...prevState.search,
+                  hint,
+                  loading: false,
+                  results: mergedResults,
+                },
+              };
             });
           });
-        }
+        });
       }
     };
   },
@@ -99,7 +108,7 @@ export default {
     actions.search.exitSearch();
   },
 
-  activeResult(state, actions, step) {
+  hoverResult(state, actions, step) {
     let results = state.search.results;
     let index = results.findIndex(result => result.active === true);
 

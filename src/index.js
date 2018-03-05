8dafcf4bd9a57841a6d94bde229daaf1111de8dd
Przemek Sobstel <przemek@sobstel.org>
2018-03-06 00:18:52 +0100

    move app to components

--- src/index.js
+++ src/index.js
@@ -3,14 +3,14 @@ import './style';
 
 let root;
 function init() {
-  let App = require('./app').default;
+  let App = require('./components/app').default;
   root = render(<App />, document.body, root);
 }
 
 // in development, set up HMR:
 if (module.hot) {
   require('preact/devtools');   // turn this on if you want to enable React DevTools!
-  module.hot.accept('./app', () => requestAnimationFrame(init) );
+  module.hot.accept('./components/app', () => requestAnimationFrame(init) );
 }
 
 init();

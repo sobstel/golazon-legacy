cb675673e82d1f7a808b5a9d95d127bf1cfc7e18
Przemek Sobstel <przemek@sobstel.org>
2019-10-09 21:43:16 +0200

    Add error page

---
+++ pages/_error.js
@@ -0,0 +1,23 @@
+import React, { Component } from "react";
+import Link from "next/link";
+import Layout from "../components/layout";
+
+export default function Error({ statusCode }) {
+  return (
+    <div className="block error__wrapper">
+      <p>
+        <strong>
+          {statusCode ? `Server error: ${statusCode}` : "Client error"}
+        </strong>
+      </p>
+      <p>
+        <Link href="/">Go home</Link>.
+      </p>
+    </div>
+  );
+}
+
+Error.getInitialProps = ({ res, err }) => {
+  const statusCode = res ? res.statusCode : err ? err.statusCode : 404;
+  return { statusCode };
+};

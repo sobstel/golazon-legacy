e49ec2ba0aeecec90fd9a976586bd1a61d5ec6db
Przemek Sobstel <przemek@sobstel.org>
2020-10-08 23:05:41 +0200

    Update cache times for api/competitions

--- pages/api/competitions.ts
+++ pages/api/competitions.ts
@@ -4,6 +4,7 @@ import getCompetitions from 'services/getCompetitions';
 export default (req: NextApiRequest, res: NextApiResponse) => {
   const result = getCompetitions(req.query.q as string);
 
-  res.setHeader('Cache-Control', 's-maxage=86400, stale-while-revalidate');
+  // maxage very high as cache is invalidated on redeploy by vercel
+  res.setHeader('Cache-Control', 'max-age=900, s-maxage=2678400, stale-while-revalidate');
   res.status(200).json(result);
 }

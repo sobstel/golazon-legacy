2937c78a120e8b0f3cac99877d29cf869e7fb468
Przemek Sobstel <przemek@sobstel.org>
2020-11-30 23:17:19 +0100

    Don't show loading spinner as it's too crowded

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -5,6 +5,7 @@ import NProgress from "nprogress";
 import "nprogress/nprogress.css";
 import "../styles/index.scss";
 
+NProgress.configure({ showSpinner: false });
 Router.events.on("routeChangeStart", () => NProgress.start());
 Router.events.on("routeChangeComplete", () => NProgress.done());
 Router.events.on("routeChangeError", () => NProgress.done());

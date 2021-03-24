a923689e5b6f80b60890386688a863ec83f009ef
Przemek Sobstel <przemek@sobstel.org>
2021-03-24 18:24:24 +0100

    Bugfix: Display logo only if results and query empty

--- components/Search/Search.tsx
+++ components/Search/Search.tsx
@@ -14,6 +14,7 @@ import {
   resetSelectedIndex,
 } from "./duck";
 import useAsyncDispatch from "./hooks/useAsyncDispatch";
+import Logo from "components/Logo";
 
 const KEY_CODES = {
   ARROW_DOWN: 40,
@@ -22,12 +23,7 @@ const KEY_CODES = {
   ENTER: 13,
 };
 
-type Props = {
-  onFocus?: () => void;
-  onBlur?: () => void;
-};
-
-export function Search({ onFocus, onBlur }: Props) {
+export function Search() {
   const [
     { query, results, loading, error, selectedIndex },
     dispatch,
@@ -36,7 +32,8 @@ export function Search({ onFocus, onBlur }: Props) {
   const asyncDispatch = useAsyncDispatch(dispatch);
   const input = useRef(null);
 
-  const clearable = query?.length > 0;
+  const hasQuery = Boolean(query?.length);
+  const hasResults = Boolean(results?.length);
 
   const handleKeyDown = ({ keyCode }) => {
     if (keyCode === KEY_CODES.ARROW_DOWN) {
@@ -67,95 +64,92 @@ export function Search({ onFocus, onBlur }: Props) {
     dispatch(queryChange(query));
     asyncDispatch(asyncSearch(query, results));
   };
-  const handleFocus = () => {
-    asyncDispatch(asyncSearch(query, results));
-    onFocus && onFocus();
-  };
-  const handleBlur = () => {
-    dispatch(resultsChange([]));
-    onBlur && onBlur();
-  };
-  const handleClear = () => {
-    dispatch(queryChange(""));
-  };
-  const handleMouseDown = (e) => {
-    // prevent input blur on click (thus clearing results before actual lick)
-    e.preventDefault();
-  };
-  const handleMouseOver = () => {
-    dispatch(resetSelectedIndex());
-  };
+  const handleFocus = () => asyncDispatch(asyncSearch(query, results));
+  const handleBlur = () => dispatch(resultsChange([]));
+  const handleClear = () => dispatch(queryChange(""));
+  // prevent input blur on click (thus clearing results before actual lick)
+  const handleMouseDown = (e) => e.preventDefault();
+  const handleMouseOver = () => dispatch(resetSelectedIndex());
   const handleClick = () => {
     dispatch(queryChange(""));
     input.current.blur();
   };
 
   return (
-    <header className="search__container" role="banner">
-      <div className="search__input-container" role="search">
-        <input
-          type="text"
-          className={classNames("search__input", {
-            "search__input--clearable": clearable,
-          })}
-          name="q"
-          placeholder="Search"
-          onKeyDown={handleKeyDown}
-          onKeyUp={handleKeyUp}
-          onChange={handleChange}
-          onFocus={handleFocus}
-          onBlur={handleBlur}
-          value={query || ""}
-          ref={input}
-          autoComplete="off"
-        />
+    <>
+      {!hasQuery && !hasResults && (
+        <Link href="/">
+          <a className="logo__container">
+            <Logo />
+          </a>
+        </Link>
+      )}
+      <header className="search__container" role="banner">
+        <div className="search__input-container" role="search">
+          <input
+            type="text"
+            className={classNames("search__input", {
+              "search__input--clearable": hasQuery,
+            })}
+            name="q"
+            placeholder="Search"
+            onKeyDown={handleKeyDown}
+            onKeyUp={handleKeyUp}
+            onChange={handleChange}
+            onFocus={handleFocus}
+            onBlur={handleBlur}
+            value={query || ""}
+            ref={input}
+            autoComplete="off"
+          />
 
-        {clearable && (
-          <button className="search__clear-button" onClick={handleClear}>
-            &#xD7;
-          </button>
-        )}
-      </div>
-      <div>
-        {error && <p className="search__error">ERROR: {error}</p>}
+          {hasQuery && (
+            <button className="search__clear-button" onClick={handleClear}>
+              &#xD7;
+            </button>
+          )}
+        </div>
+        <div>
+          {error && <p className="search__error">ERROR: {error}</p>}
 
-        <ul className="search__results">
-          {results.map((result, i) => (
-            <li key={result["competition_id"]}>
-              <Link href={`/c/${result["competition_id"]}`}>
-                <a
-                  onMouseDown={handleMouseDown}
-                  onMouseOver={handleMouseOver}
-                  onClick={handleClick}
-                  className={classNames({ active: selectedIndex === i })}
-                >
-                  <Highlighter
-                    highlightClassName="search__highlight"
-                    searchWords={[query]}
-                    autoEscape={false}
-                    textToHighlight={result.name}
-                  />{" "}
-                  {result["area_name"] && (
-                    <span>
-                      (
-                      <Highlighter
-                        highlightClassName="search__highlight"
-                        searchWords={[query]}
-                        autoEscape={true}
-                        textToHighlight={result["area_name"]}
-                      />
-                      ){" "}
-                    </span>
-                  )}
-                  {result.teamtype}
-                </a>
-              </Link>
-            </li>
-          ))}
+          <ul className="search__results">
+            {results.map((result, i) => (
+              <li key={result["competition_id"]}>
+                <Link href={`/c/${result["competition_id"]}`}>
+                  <a
+                    onMouseDown={handleMouseDown}
+                    onMouseOver={handleMouseOver}
+                    onClick={handleClick}
+                    className={classNames({ active: selectedIndex === i })}
+                  >
+                    <Highlighter
+                      highlightClassName="search__highlight"
+                      searchWords={[query]}
+                      autoEscape={false}
+                      textToHighlight={result.name}
+                    />{" "}
+                    {result["area_name"] && (
+                      <span>
+                        (
+                        <Highlighter
+                          highlightClassName="search__highlight"
+                          searchWords={[query]}
+                          autoEscape={true}
+                          textToHighlight={result["area_name"]}
+                        />
+                        ){" "}
+                      </span>
+                    )}
+                    {result.teamtype}
+                  </a>
+                </Link>
+              </li>
+            ))}
 
-          {loading && <li className="search__loader loader">loading</li>}
-        </ul>
-      </div>
-    </header>
+            {loading && <li className="search__loader loader">loading</li>}
+          </ul>
+        </div>
+      </header>
+    </>
   );
 }

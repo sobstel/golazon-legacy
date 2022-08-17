5f0f2226fc5a1dc5d8a7946827b5107ded2b3713
Przemek Sobstel <przemek@sobstel.dev>
2022-08-17 13:56:56 +0200

    use experimental-edge for api

--- pages/api/index.ts
+++ pages/api/index.ts
@@ -3,3 +3,7 @@ import { NextApiRequest, NextApiResponse } from "next";
 export default async (_req: NextApiRequest, res: NextApiResponse) => {
   res.redirect(308, "/developer/");
 };
+
+export const config = {
+  runtime: "experimental-edge",
+};
